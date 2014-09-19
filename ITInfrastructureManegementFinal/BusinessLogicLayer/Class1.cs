e7f446bff4fe1669b0419ba9de040404006b2c16
sushantsakurde11 <sushantsakurde11@gmail.com>
2014-09-19 15:00:29 +0530

    testing

---
+++ ITInfrastructureManegementFinal/BusinessLogicLayer/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemObject
+{
+    class Class1
+    {
+        string s = "hwllo thwrw:";
+
+    }
+}
